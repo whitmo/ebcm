f6e8f7e0cbbd6f12d9814158d7eb39c8f467ea40
Whit Morriss <whit@openplans.org>
2009-07-21 22:03:59 -0500

    rm

--- ebblog/ebblog/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.admin import ModelAdmin, site
-from ebblog.blog.models import Entry
-
-class EntryAdmin(ModelAdmin):
-    list_display = ('pub_date', 'headline', 'author')
-
-site.register(Entry, EntryAdmin)
